556477c50290d06cd365c06c8f35d9b4391c036a
Pragya2000 <68546970+Pragya2000@users.noreply.github.com>
2021-03-07 20:06:49 +0530

    Add files via upload

---
+++ eeg-brainwave-final/eeg-app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+app = Flask(__name__)
+
+MODEL_PATH='eeg-brainwave-pred.h5'
+model =tf.keras.models.load_model(MODEL_PATH)
+
+@app.route('/index.html')
+def home_page():
+	return render_template('index.html')
+
+@app.route('/about.html')
+def about_page():
+	return render_template('about.html')
+
+@app.route('/uploadx.html')
+def upload_file1():
+   return render_template('uploadx.html')
+
+
+def predict(df):
+    X= df.drop('2548', axis=1)
+    A=model.predict(X)
+    num=np.argmax(A)
+    if(num==0):
+      return 'NEGATIVE'
+    elif(num==1):
+      return 'NEUTRAL'
+    elif(num==2):
+      return 'POSITIVE'
+
+import requests
+
+
+@app.route('/result.html', methods = ['GET', 'POST'])
+def upload_file2():
+   if request.method == 'POST':
+      f = request.files['file']
+      name=request.form.get('first_name')
+      mail=request.form.get('mail_id')
+      df=pd.read_csv(f)
+      emotion=predict(df)
+      response = requests.post('https://events-api.notivize.com/applications/d5f045ef-7789-400f-a2f6-20a086899fb4/event_flows/c8fe106a-69db-4195-afe3-7c8f4dcedb9b/events', json = {
+      'email': mail,
+      'first_name': name,
+      'result': 0,
+      'unique_id': 123456
+      })
+      print(response)
+      
+      return render_template('result.html',emotion=emotion)
+        
+if __name__ == '__main__':
+   app.run(debug = True)
